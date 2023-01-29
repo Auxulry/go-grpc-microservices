9cf9badc876290e5b0aaf220a2979c2f9d910489
Mochamad Akbar <mochakbar912@gmail.com>
2023-01-29 10:32:22 +0700

    chore(deps): add viper

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"github.com/MochamadAkbar/go-grpc-microservices/cmd"
+	"github.com/MochamadAkbar/go-grpc-microservices/configs"
 )
+
+func init() {
+	cfg, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(cfg)
+}
 
 func main() {
 	cmd.Execute()
---
+++ configs/configs.go
@@ -0,0 +1,22 @@
+package configs
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	JWTSecret string `mapstructure:"JWT_SECRET"`
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName(".env")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
